ed42c07d77c51a9862a6e286081368e577ea2f57
moisesliotawork <moises.liotawork@gmail.com>
2025-06-16 15:26:27 -0400

    Resource Management Depto :sparkles:

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,8 +9,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RoleSeeder::class,
             UserSeeder::class,
-            // Otros seeders
         ]);
     }
 }
